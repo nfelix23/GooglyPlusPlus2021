02b2f2a7259eb48bfcab14ae78ae4dc4ca060082
Tinniam V Ganesh <tvganesh@Apples-MacBook-Pro.local>
2021-01-24 12:47:47 +0530

    Initial

---
+++ definitions.R
@@ -0,0 +1,98 @@
+#####################################################################################################
+#
+# Title :  GooglyPlusPLus - An interactive app to analyze T20 and ODI matches
+# Designed and developed by: Tinniam V Ganesh
+# Date : 07 Jan 2021
+# File: definitions.R
+# More details: https://gigadom.in/
+#
+#########################################################################################################
+# Function names for IPL  batsman analysis
+batsmanFuncs <- c("Batsman Runs vs. Deliveries",
+                       "Dismissals of batsman",
+                       "Batsman's Runs vs Strike Rate",
+                       "Batsman's Moving Average",
+                       "Batsman's Cumulative Average Runs",
+                       "Batsman's Cumulative Strike Rate",
+                       "Batsman's Runs against Opposition",
+                       "Batsman's  Runs at Venue",
+                       "Predict Runs of batsman")
+
+
+
+# Function names for IPL bowler analysis
+bowlerFuncs <- c("Mean Economy Rate of bowler",
+                     "Mean runs conceded by bowler",
+                     "Bowler's Moving Average",
+                     "Bowler's Cumulative Avg. Wickets",
+                     "Bowler's Cumulative Avg. Economy Rate",
+                     "Bowler's Wicket Plot",
+                     "Bowler's Wickets against opposition",
+                     "Bowler's Wickets at Venues",
+                     "Bowler's wickets prediction")
+
+
+# Functions to analyze IPL matches
+matchFuncs <- c("Match Batting Scorecard",
+                   "Batting Partnerships",
+                   "Batsmen vs Bowlers",
+                   "Bowling Wicket Kind",
+                   "Match Bowling Scorecard",
+                   "Bowling Wicket Runs",
+                   "Bowling Wicket Match",
+                   "Bowler vs Batsmen",
+                   "Match Worm Graph")
+
+
+# Functions to analyze IPL matches
+matches2TeamsFuncs <- c("Team Batsmen Batting Partnerships All Matches",
+                           "Team Batting Scorecard All Matches",
+                            "Team Batsmen vs Bowlers all Matches",
+                            "Team Wickets Opposition All Matches",
+                           "Team Bowling Scorecard All Matches",
+                            "Team Bowler vs Batsmen All Matches",
+                            "Team Bowlers Wicket Kind All Matches",
+                            "Team Bowler Wicket Runs All Matches",
+                            "Win Loss All Matches")
+
+# Functions to analyze IPL teams' overall performance
+teamOverallPerfFunc <- c("Team Batsmen Partnerships Overall",
+                            "Team Batting Scorecard Overall",
+                           "Team Batsmen vs Bowlers Overall",
+                           'Team Bowler vs Batsmen Overall',
+                           "Team Bowling Scorecard Overall",
+                           "Team Bowler Wicket Kind Overall")
+
+
+# Rank IPL
+rankIPLPlayerFuncs <- c("Rank IPL batsmen",
+                        "Rank IPL bowlers")
+
+# Rank T20 (Men)
+rankT20MPlayerFuncs <- c("Rank T20 batsmen (Men)",
+                        "Rank T20 bowlers (Men)")
+# Rank T20 (Women)
+rankT20WPlayerFuncs <- c("Rank T20 batsmen (Women)",
+                         "Rank T20 bowlers (Women)")
+
+# Rank BBK
+rankBBLPlayerFuncs <- c("Rank BBL batsmen",
+                        "Rank BBL bowlers")
+
+# Rank NTB
+rankNTBPlayerFuncs <- c("Rank NTB batsmen",
+                        "Rank NTB bowlers")
+
+# Rank PSL
+rankPSLPlayerFuncs <- c("Rank PSL batsmen",
+                        "Rank PSL bowlers")
+
+# Rank WBB
+rankWBBPlayerFuncs <- c("Rank WBB batsmen",
+                        "Rank WBB bowlers")
+
+# Runs vs Strike Rate
+runsVsSR = c("Runs over SR", "SR over Runs")
+
+# Wickets vs Economy Rate
+wicketsVsER = c("Wickets over ER", "ER over Wickets")
---
+++ rankPlayers.R
@@ -0,0 +1,78 @@
+#####################################################################################################
+#
+# Title :  GooglyPlusPLus - An interactive app to analyze T20 and ODI matches
+# Designed and developed by: Tinniam V Ganesh
+# Date : 07 Jan 2021
+# File: rankPlayers.R
+# More details: https://gigadom.in/
+#
+#########################################################################################################
+rankPlayers <- function(input,output,type="IPL") {
+    cat("Entering rank match")
+
+  if (type == "IPL"){
+    output$Mode <- renderUI({
+        if (input$rankFuncIPL == "Rank IPL batsmen"){
+          selectInput('runsOverSR', 'Mode', runsVsSR)
+        } else if (input$rankFuncIPL == "Rank IPL bowlers"){
+          selectInput('wicketsOverER', 'Mode', wicketsVsER)
+        }
+    })
+  }
+
+  if(type == "IPL") {
+
+        if(input$rankFuncIPL == "Rank IPL batsmen"){
+          cat("#######=",input$years,input$runsOverSR)
+            a <-rankIPLBatsmen("./ipl/iplBattingBowlingDetails",input$minMatches, input$years,input$runsOverSR)
+        } else if (input$rankFuncIPL == "Rank IPL bowlers"){
+           a <- rankIPLBowlers("./ipl/iplMatches","./ipl/iplBattingBowlingDetails",input$minMatches)
+
+        }
+  } else if (type ==  "T20M"){
+      if(input$rankFuncT20M == "Rank T20 batsmen (Men)"){
+          a <- rankT20Batsmen("./t20/t20MenMatches","./t20/t20BattingBowlingDetails",input$minMatchesT20M)
+      } else if (input$rankFuncT20M == "Rank T20 bowlers (Men)"){
+          a <-rankT20Bowlers("./t20/t20MenMatches","./t20/t20BattingBowlingDetails",input$minMatchesT20M)
+
+      }
+  } else if (type ==  "T20W"){
+      if(input$rankFuncT20W == "Rank T20 batsmen (Women)"){
+          a <- rankT20Batsmen("./t20/t20WomenMatches","./t20/t20WomenBattingBowlingDetails",input$minMatchesT20W)
+      } else if (input$rankFuncT20W == "Rank T20 bowlers (Women)"){
+          a <- rankT20Bowlers("./t20/t20WomenMatches","./t20/t20WomenBattingBowlingDetails",input$minMatchesT20W)
+
+      }
+  } else if (type ==  "BBL"){
+      if(input$rankFuncBBL == "Rank BBL batsmen"){
+          a <- rankBBLBatsmen("./bbl/bblMatches","./bbl/bblBattingBowlingDetails",input$minMatchesBBL)
+      } else if (input$rankFuncBBL == "Rank BBL bowlers"){
+          a <- rankBBLBowlers("./bbl/bblMatches","./bbl/bblBattingBowlingDetails",input$minMatchesBBL)
+
+      }
+  } else if (type ==  "NTB"){
+      if(input$rankFuncNTB == "Rank NTB batsmen"){
+          a <- rankNTBBatsmen("./ntb/ntbMatches","./ntb/ntbBattingBowlingDetails",input$minMatchesNTB)
+      } else if (input$rankFuncNTB == "Rank NTB bowlers"){
+          a <- rankNTBBowlers("./ntb/ntbMatches","./ntb/ntbBattingBowlingDetails",input$minMatchesNTB)
+
+      }
+  } else if (type ==  "PSL"){
+      if(input$rankFuncPSL == "Rank PSL batsmen"){
+          a <- rankPSLBatsmen("./psl/pslMatches","./psl/pslBattingBowlingDetails",input$minMatchesPSL)
+      } else if (input$rankFuncPSL == "Rank PSL bowlers"){
+          a <- rankPSLBowlers("./psl/pslMatches","./psl/pslBattingBowlingDetails",input$minMatchesPSL)
+          print(a)
+
+      }
+  } else if (type ==  "WBB"){
+      if(input$rankFuncWBB == "Rank WBB batsmen"){
+          a <- rankWBBBatsmen("./wbb/wbbMatches","./wbb/wbbBattingBowlingDetails",input$minMatchesWBB)
+      } else if (input$rankFuncWBB == "Rank WBB bowlers"){
+          a <- rankWBBBowlers("./wbb/wbbMatches","./wbb/wbbBattingBowlingDetails",input$minMatchesWBB)
+          print(a)
+
+      }
+  }
+    a
+}
